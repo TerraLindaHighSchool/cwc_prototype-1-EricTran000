70bd030b93643df529c505362c38ae982e072242
EricTran000 <89489669+EricTran000@users.noreply.github.com>
2022-03-29 17:00:43 -0500

    Finished Plane Challenge

---
+++ Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+   
+
+    // Rotates the propeller
+    void Update()
+    {
+        transform.Rotate(new Vector3(0,0,700) * Time.deltaTime);
+    }
+}
